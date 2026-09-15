d6ef73adc3c1b349233a9beace062ac20c7a64be
NobreadBreadless <zachsubala46@gmail.com>
2026-09-14 22:42:40 -0700

    done

--- lab3-Matrix/Matrix.java
+++ lab3-Matrix/Matrix.java
@@ -1,15 +1,86 @@
+import java.util.*;
+
 public class Matrix {
     private int[][] data;
+    private int rows;
+    private int cols;
 
     public Matrix(int rows, int cols) {
         this.data = new int[rows][cols];
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    public Matrix(int[][] data) {
+        this.data = data;
+        this.rows = data.length;
+        this.cols = data[0].length;
     }
 
     public void populateRandom() {
-
+        for (int i = 0; i < data.length; i++) {
+            for(int j = 0; j < data[i].length; j++) {
+                data[i][j] = (int) ((Math.random() * 10) + 1);
+            }
+        }
     }
 
     public Matrix add(Matrix other) {
-        
+        if ((this.data.length == other.data.length) && (this.data[0].length == other.data[0].length)) {
+            Matrix newData = new Matrix(rows, cols);
+
+            for (int i = 0; i < data.length; i++) {
+                for (int j = 0; j < data[0].length; j++) {
+                    newData.data[i][j] = data[i][j] + other.data[i][j];
+                }
+            }
+
+            return newData;
+        } else {
+            throw new IllegalArgumentException("Sowwy, both rows and columns of both matrices have to be equal :(");
+        }
+    }
+
+    public Matrix multiply(Matrix other) {
+        if (data[0].length == other.data.length) {
+            Matrix newData = new Matrix(this.data.length, other.data[0].length);
+            int sum = 0;
+
+            for (int row = 0; row < newData.data.length; row++) {
+                for (int col = 0; col < newData.data[0].length; col++) {
+
+                    //Multiplying loop
+                    for (int k = 0; k < this.data[0].length; k++) {
+                        sum += this.data[row][k] * other.data[k][col];
+                    }
+
+                    newData.data[row][col] = sum;
+
+                    sum = 0;
+                }
+            }
+
+            return newData;
+        } else {
+            throw new IllegalArgumentException("Sowwy matrix 1's columns don't equal matrix 2's rows");
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                sb.append(data[i][j]);
+
+                //tab for better format
+                sb.append("\t");
+            }
+
+                sb.append("\n"); 
+            }
+
+            return sb.toString();
     }
 }
--- lab3-Matrix/MatrixMain.java
+++ lab3-Matrix/MatrixMain.java
@@ -1,3 +1,46 @@
+import java.util.Scanner;
+
 public class MatrixMain {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        //THE Matrix
+        System.out.print("Let's make a matrix!\nEnter number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = scanner.nextInt();
+
+        Matrix matrix = new Matrix(rows, cols);
+
+
+        //Other Matrix
+        System.out.print("Let's make a matrix!\nEnter number of rows: ");
+        int rows2 = scanner.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols2 = scanner.nextInt();
+
+        Matrix otherMatrix = new Matrix(rows2, cols2);
+
+
+        //Fill up these matrices
+        matrix.populateRandom();
+        otherMatrix.populateRandom();
+
+        System.out.println("Your current matrices: ");
+        System.out.println(matrix.toString() + "\n" + otherMatrix.toString());
+
+        
+        //Add or multiply
+        System.out.print("Wanna add them or multiply?: ");
+        String ans = scanner.next();
+
+        if (ans.equalsIgnoreCase("Add")) {
+            System.out.println(matrix.add(otherMatrix).toString());
+        } else if (ans.equalsIgnoreCase("Multiply")) {
+            System.out.println(matrix.multiply(otherMatrix));
+        } else {
+            throw new IllegalArgumentException("Sowwy u can only add or multiply");
+        }
+    }
 }
