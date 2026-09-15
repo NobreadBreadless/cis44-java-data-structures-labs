ff8f6b7be49e20abb927439ab4cdb4163a0809a2
NobreadBreadless <zachsubala46@gmail.com>
2026-09-14 17:05:31 -0700

    final

--- lab3-Matrix/Matrix.java
+++ lab3-Matrix/Matrix.java
@@ -1,5 +1,15 @@
 public class Matrix {
     private int[][] data;
 
-    
+    public Matrix(int rows, int cols) {
+        this.data = new int[rows][cols];
+    }
+
+    public void populateRandom() {
+
+    }
+
+    public Matrix add(Matrix other) {
+        
+    }
 }
